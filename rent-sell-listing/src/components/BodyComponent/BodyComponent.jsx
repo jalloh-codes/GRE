804bb9c4e04ea635cc26d2b7fc4be80b6e4b0ab8
souleymaneGit <thiernosouleymane24@gmail.com>
2022-04-15 20:43:58 -0400

    change the rent-listing to rent-sell-listing

---
+++ rent-sell-listing/src/components/BodyComponent/BodyComponent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./BodyComponent.css"
+
+export const BodyComponent = () => {
+    return (
+        <div className="Body_contain">
+
+            I am the rent listing
+
+        </div>
+    );
+}
---
+++ rent-sell-listing/src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './Header.css';
+
+export const Header = () => {
+    return (
+        <div className="head_top_contains">
+
+        </div>
+    );
+}
