683eae6c26582a25427a1996184c30fe24627b30
Saylu Jalloh <saylujalloh@gmail.com>
2022-12-29 15:51:19 -0500

    remove log

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -19,7 +19,7 @@ import {  ApolloClient, InMemoryCache, from, ApolloLink,
   ApolloProvider, HttpLink, concat } from "@apollo/client";
 import {onError} from '@apollo/client/link/error';
 import {ResetPassword} from './Components/ResetPassword/ResetPassword';
-import {api_link, environment} from './environment'
+import {api_link} from './environment'
   
 const errorLink = onError(({ graphQLErrors, networkError }) => {
     if (graphQLErrors)
@@ -72,8 +72,7 @@ function App() {
   useEffect(() =>{
     setAuthanticated(token.authanticated)
   },[token, authanticated])
-  console.log(api_link);
-  console.log(environment);
+
   return (
     <authContext.Provider value={{authanticated, setAuthanticated}}>
     <ApolloProvider  client={client}>
